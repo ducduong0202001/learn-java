3ba8e88e090f14082cf0ff4f23fbd010819d48c5
Pham Van Duc <ducduong0202001@gmail.com>
2024-04-05 10:52:01 +0700

    mergersort

--- Learn_java/src/Main.java
+++ Learn_java/src/Main.java
@@ -1,3 +1,5 @@
+import DataStructure.BubbleSort;
+import DataStructure.MergeSort;
 import OOP.Cat;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,14 +8,33 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        Cat cat= new Cat();
-        cat.setGiongLoai("Meo");
-        cat.setSoChan(4);
-        cat.setLoaiThucAn("thit, rau, cu");
-        cat.setMauLong("den");
-        cat.getInfo(cat.getGiongLoai(), cat.getSoChan(), cat.getLoaiThucAn(), cat.getMauLong());
-        cat.getInfoMove();
-        cat.getWeight(6);
-        cat.getSound("meomeo");
+        // Demo về OOP
+//        Cat cat= new Cat();
+//        cat.setGiongLoai("Meo");
+//        cat.setSoChan(4);
+//        cat.setLoaiThucAn("thit, rau, cu");
+//        cat.setMauLong("den");
+//        cat.getInfo(cat.getGiongLoai(), cat.getSoChan(), cat.getLoaiThucAn(), cat.getMauLong());
+//        cat.getInfoMove();
+//        cat.getWeight(6);
+//        cat.getSound("meomeo");
+
+        //Tìm kiếm tuần tự
+//        int[] arr = {1, 5, 4, 8, 2, 3, 10, 9, 6, 7};
+//        BubbleSort sort = new BubbleSort();
+//        sort.bubSort(arr);
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            System.out.print(arr[i]);
+//        }
+        int[] arr = {1, 5, 4, 8, 2, 3, 10, 9, 6, 7};
+        MergeSort sort = new MergeSort();
+        System.out.println("Mảng ban đầu:");
+        sort.printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nMảng đã sắp xếp:");
+        sort.printArray(arr);
     }
 }
